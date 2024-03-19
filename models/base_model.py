efff4ec219d620a59753d45c3f8e8595496b90b2
zyadhany <zeyad.hany2003@gmail.com>
2024-03-20 01:52:34 +0200

    saged #6

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,6 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    if models.storage_t == "db":
-        id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime,nullable=False, default=datetime.utcnow())
-        updated_at = Column(DateTime,nullable=False, default=datetime.utcnow())
-
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
