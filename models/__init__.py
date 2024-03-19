cf98a174122d7c6c47b67b6df288f4f9d617f4bc
zyadhany <zeyad.hany2003@gmail.com>
2024-03-20 01:45:10 +0200

    saged #6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from os import getenv
 
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
@@ -8,12 +9,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
 class BaseModel:
     """A base class for all hbnb models"""
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime,nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime,nullable=False, default=datetime.utcnow())
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True, nullable=False)
+        created_at = Column(DateTime,nullable=False, default=datetime.utcnow())
+        updated_at = Column(DateTime,nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
