ba8b52e613365b9c7aee0ea2664124417c61ce5a
zyadhany <zeyad.hany2003@gmail.com>
2024-03-20 02:05:48 +0200

    solved task

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 import models
 from datetime import datetime
+from models import storage
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,6 +13,10 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime,nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime,nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -19,7 +24,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -33,10 +37,14 @@ class BaseModel:
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
+    def delete(self):
+        """delete obj from storage"""
+        storage.delete(self)
+
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -47,4 +55,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         return dictionary
