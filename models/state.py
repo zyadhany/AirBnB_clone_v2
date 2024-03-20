2ef0c3776627ca3f84f3d08c0e65d3c8d3fdb3a3
zyadhany <zeyad.hany2003@gmail.com>
2024-03-20 14:58:58 +0200

    task 6

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import models
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
