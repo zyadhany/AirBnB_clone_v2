c516926dfc5ae2ecf22d85073ca0982443859f3f
zyadhany <zeyad.hany2003@gmail.com>
2024-04-21 00:55:49 +0200

    solved task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Start with flask """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def root():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_cool(text='is cool'):
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
