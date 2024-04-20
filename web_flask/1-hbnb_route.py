00f5eaf7628e7155a15ed59edea1cb09b1d21329
zyadhany <zeyad.hany2003@gmail.com>
2024-04-21 00:41:33 +0200

    solved task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-from flask import Flask, flash, redirect, render_template, request, session, send_file
-from flask_session import Session
+
+""" Start with flask """
+
+from flask import Flask
 
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.route("/", strict_slashes=False)
 def root():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
